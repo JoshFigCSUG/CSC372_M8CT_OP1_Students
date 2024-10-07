8a8f888284085855f7ca7804357f092ae4d7013b
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-10-06 20:32:43 -0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,45 @@
 public class Student {
-    
+
+    private String name;
+    private String address;
+    private double GPA;
+
+    // Constructor
+    public Student(String name, String address, double GPA) {
+        this.name = name;
+        this.address = address;
+        this.GPA = GPA;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setGPA(Double GPA) {
+        this.GPA = GPA;
+    }
+
+    // Override default toString method to a formatted string of Student object
+    @Override
+    public String toString() {
+        return "Student [Name: " + name + ", Address: " + address + ", GPA: " + GPA + "]";
+    }
 }
